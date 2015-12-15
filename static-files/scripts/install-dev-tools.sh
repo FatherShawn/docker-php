0cfe55ec62cf1f3ce4e634b3077b940c54c49a0c
Yosh de Vos <yosh@insiders.nl>
2015-12-15 17:54:43 +0100

    Devtools: removed uglify, downgraded drush to 6.5 and fixed missing symfony2 code styling

--- static-files/scripts/install-dev-tools.sh
+++ static-files/scripts/install-dev-tools.sh
@@ -11,24 +11,26 @@ apt-get -yq install --no-install-recommends \
 # Configure ruby and install bundler and sass
 update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby2.0 1
 update-alternatives --install /usr/bin/gem gem /usr/bin/gem2.0 1
-gem install rubygems-update bundler sass
+gem install bundler sass
 
 # Install nodejs
 curl -sL https://deb.nodesource.com/setup | bash -
 apt-get -yq install nodejs
 
 # Install bower, grunt and gulp
-npm install -g bower grunt-cli gulp uglify-js uglifycss
+npm install -g bower grunt-cli gulp
 
 # Install drush, phing, phpunit and phpcs
-composer global require drush/drush:7.* phing/phing:2.* phpunit/phpunit:4.* squizlabs/php_codesniffer
+composer global require drush/drush:6.5.* phing/phing:2.* phpunit/phpunit:4.* squizlabs/php_codesniffer
 ln -s $COMPOSER_HOME/vendor/drush/drush/drush /usr/local/bin/drush
 ln -s $COMPOSER_HOME/vendor/bin/phing /usr/local/bin/phing
 ln -s $COMPOSER_HOME/vendor/bin/phpunit /usr/local/bin/phpunit
 ln -s $COMPOSER_HOME/vendor/bin/phpcs /usr/local/bin/phpcs
 
 # Add symfony2 code styling
-git clone git://github.com/escapestudios/Symfony2-coding-standard.git $COMPOSER_HOME/vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/Symfony2
+git clone git://github.com/escapestudios/Symfony2-coding-standard.git /tmp/Symfony2-coding-standard
+cp -a /tmp/Symfony2-coding-standard/Symfony2 $COMPOSER_HOME/vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/
+rm -rf /tmp/Symfony2-coding-standard
 
 # Add drupal code styling
 git clone --branch 7.x-2.x http://git.drupal.org/project/coder.git /tmp/coder
